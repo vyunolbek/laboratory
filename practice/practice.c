bbb0d6672fc8f1b410eced0a4a551cb0fb05c4bb
vyunolbek <cliffordik14@gmail.com>
2021-10-14 19:26:49 +0300

    first commit

---
+++ practice/practice.c
@@ -0,0 +1,35 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#define ARR_SIZE 100000     // Размер массива
+
+const float RAND_MAX_F = RAND_MAX;
+
+float get_rand() {      // Генератор рандомного числа
+    return rand() / RAND_MAX_F;
+}
+
+float get_rand_range(const float min, const float max) {         // Генератор рандомного числа в диапазоне
+    return get_rand() * (max - min) + min;
+}
+
+int main() {
+    float minimum, maximum, arr[ARR_SIZE];
+    int ost, num, sum = 0;
+    scanf_s("%f", &minimum);      // Рандомное число от
+    scanf_s("%f", &maximum);      // Рандомное число до
+    for (int i = 0; i < ARR_SIZE; i++) {        // Заполняем массив
+        arr[i] = get_rand_range(minimum, maximum);
+    }
+    for (int j = 0; j < ARR_SIZE; j++) {        // Отделяем дробную часть и проверяем
+        num = arr[j];
+        ost = (arr[j] - num) * 1000000;
+        if (ost < ARR_SIZE) {         // Проверка на существование индекса в массиве
+            sum -= arr[j];
+        }
+        else {
+            sum += arr[j];
+        }
+    }
+    printf("%d", sum);
+    return 0;
+}
